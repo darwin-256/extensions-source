f8a69b3f3065414663d6a37183fa0019754755f4
darwin-256 <viktorasordinass@gmail.com>
2026-02-28 10:19:40 +0200

    Add FilterList import to StoneScape.kt

--- src/en/stonescape/src/eu/kanade/tachiyomi/extension/en/stonescape/StoneScape.kt
+++ src/en/stonescape/src/eu/kanade/tachiyomi/extension/en/stonescape/StoneScape.kt
@@ -1,6 +1,7 @@
 package eu.kanade.tachiyomi.extension.en.stonescape
 
 import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
